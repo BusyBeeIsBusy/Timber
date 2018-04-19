90caf72266c895f8dff21d1f041c792fd12dc922
Darius Perry <DarJPerry@gmail.com>
2018-04-18 23:40:54 -0400

    new files structure and basic functionallity added

---
+++ resources/js/index.js
@@ -0,0 +1,123 @@
+$(document).ready(function () {
+
+    function SetSwipePicture(parkIndex, pictureIndex) {
+        $(".parkImage").css("background-image", "url(resources/images/park-pictures/" + RawParkList[0].images[pictureIndex] + ".jpg)");
+
+    }
+
+    var RawParkList = new Array();
+    var YesList = new Array();
+    var NoList = new Array();
+    var fileInforamtion = "";
+    var parkInfoFileLocation = "resources/data/park-info.json"
+    var currentParkIndex = 0;
+    var currentParkPicIndex = 0
+    var yesParkIndex = 0;
+
+
+
+
+    $.ajax({
+        url: parkInfoFileLocation,
+        dataType: 'json',
+        async: false,
+        success: function (data) {
+            console.log(data)
+            fileInforamtion = data;
+        }
+    });
+
+    fileInforamtion.forEach(function (park) {
+        RawParkList.push(park);
+    });
+
+    $("#myParkNum").html(YesList.length);
+    $("#newParkNum").html(RawParkList.length)
+
+    SetSwipePicture(currentParkIndex, currentParkPicIndex);
+    $("#parkName").html(RawParkList[0].name)
+
+
+
+    $("#find #right-arrow").click(function () {
+        currentParkPicIndex++;
+        SetSwipePicture(currentParkIndex, currentParkPicIndex);
+
+    })
+
+    $("#find #left-arrow").click(function () {
+        currentParkPicIndex--;
+        SetSwipePicture(currentParkIndex, currentParkPicIndex);
+    })
+
+    $("#yes").click(function () {
+        YesList.push(RawParkList[currentParkIndex]);
+        (RawParkList.splice(currentParkIndex, 1));
+
+        currentParkPicIndex = 0;
+        SetSwipePicture(0, currentParkPicIndex);
+
+        // currentPark Index Never Changes?
+        $("#myParkNum").html(YesList.length);
+        $("#newParkNum").html(RawParkList.length)
+
+        console.log(RawParkList)
+        console.log(currentParkIndex)
+        $("#parkName").html(RawParkList[0].name)
+        $("#parkName").html(RawParkList[0].name)
+
+
+
+        if ($("#yesParkName").html() == "") {
+            $("#yesParkName").html(YesList[0].name)
+            $("#openTime").html(YesList[0].open_hour)
+            $("#closingTime").html(YesList[0].close_hour);
+            $("#location").html(YesList[0].address + ", " + YesList[0].city + ", " + YesList[0].state + " " + YesList[0].zip)
+            $("#price").html(YesList[0].price);
+            $(".yesParkImage").css("background-image", "url(resources/images/park-pictures/" + YesList[0].images[0] + ".jpg)");
+        }
+
+
+    })
+
+    $("#no").click(function () {
+        NoList.push(RawParkList[currentParkIndex]);
+        (RawParkList.splice(currentParkIndex, 1));
+
+        currentParkPicIndex = 0;
+        SetSwipePicture(0, currentParkPicIndex);
+
+        // currentPark Index Never Changes?
+        $("#myParkNum").html(YesList.length);
+        $("#newParkNum").html(RawParkList.length)
+        console.log(RawParkList)
+        console.log(currentParkIndex)
+
+        $("#parkName").html(RawParkList[0].name)
+
+        // get rid of current park index, always 0
+
+    })
+
+    $("#myParks #right-arrow").click(function () {
+        yesParkIndex++;
+
+        $("#yesParkName").html(YesList[yesParkIndex].name)
+        $("#openTime").html(YesList[yesParkIndex].open_hour)
+        $("#closingTime").html(YesList[yesParkIndex].close_hour);
+        $("#location").html(YesList[yesParkIndex].address + ", " + YesList[yesParkIndex].city + ", " + YesList[yesParkIndex].state + " " + YesList[yesParkIndex].zip)
+        $("#price").html(YesList[yesParkIndex].price);
+        $(".yesParkImage").css("background-image", "url(resources/images/park-pictures/" + YesList[yesParkIndex].images[0] + ".jpg)");
+    })
+
+    $("#myParks #left-arrow").click(function () {
+        yesParkIndex--;
+
+        $("#yesParkName").html(YesList[yesParkIndex].name)
+        $("#openTime").html(YesList[yesParkIndex].open_hour)
+        $("#closingTime").html(YesList[yesParkIndex].close_hour);
+        $("#location").html(YesList[yesParkIndex].address + ", " + YesList[yesParkIndex].city + ", " + YesList[yesParkIndex].state + " " + YesList[yesParkIndex].zip)
+        $("#price").html(YesList[yesParkIndex].price);
+        $(".yesParkImage").css("background-image", "url(resources/images/park-pictures/" + YesList[yesParkIndex].images[0] + ".jpg)");
+    })
+})
